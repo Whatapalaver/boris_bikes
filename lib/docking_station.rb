818c16bfe982cfdb4c68ed125695efd04841e10a
Angela Wolff <warriorwomenblog@gmail.com>
2018-07-12 15:36:39 +0100

    Fixed the failing bike creation error

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,9 +1,10 @@
-require_relative "Bike"
+require_relative './bike.rb'
 
 class DockingStation
 	attr_reader :bike
 	def release_bike
-		@bike
+		Bike.new
+		# @bike
 	end
 	def dock(bike)
 		@bike = bike
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,7 +5,13 @@ describe DockingStation do
 	# method tests
 	it { is_expected.to respond_to :release_bike }
 	it { is_expected.to respond_to(:dock).with(1).argument }
-	it { is_expected.to respond_to :bike }
+  it { is_expected.to respond_to :bike }
+  
+  it 'releases working bikes' do
+    bike = Bike.new
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
 
   # docking tests
 
